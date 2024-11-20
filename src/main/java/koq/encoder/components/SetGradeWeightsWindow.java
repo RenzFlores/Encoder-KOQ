c19672a1536866636fdc4995af5b648a78b52831
RenzFlores <60131895+RenzFlores@users.noreply.github.com>
2024-11-20 15:52:54 +0800

    Update SetGradeWeightsWindow.java

--- src/main/java/koq/encoder/components/SetGradeWeightsWindow.java
+++ src/main/java/koq/encoder/components/SetGradeWeightsWindow.java
@@ -9,87 +9,85 @@ import javax.swing.JTextField;
 
 public class SetGradeWeightsWindow extends JFrame {
     
-    private JLabel jLabel1;
-    private JLabel jLabel3;
-    private JLabel jLabel2;
+    private JLabel writtenWorksLabel;
     private JLabel jLabel19;
-    private JLabel jLabel14;
-    private JLabel jLabel18;
-    private JLabel jLabel11;
-    private JLabel jLabel20;
-    private JLabel jLabel12;
+    private JLabel performanceTaskLabel;
     private JLabel jLabel17;
-    private JLabel jLabel13;
+    private JLabel quarterlyAssessmentLabel;
     private JLabel jLabel16;
     
-    private JTextField jTextField1;
-    private JTextField jTextField9;
-    private JTextField jTextField2;
-    private JTextField jTextField7;
-    private JTextField jTextField8;
-    
-    private JSeparator jSeparator1;
-    private JSeparator jSeparator2;
+    private JTextField wwWeightField;
+    private JTextField ptWeightField;
+    private JTextField qaWeightField;
+
     private JSeparator jSeparator3;
     
-    private JButton jButton1;
+    private JButton confirmButton;
     
     public SetGradeWeightsWindow() {
         setLayout(new FlowLayout(FlowLayout.CENTER));
         
-        jLabel1 = new JLabel("Set grade weights for this class record:");
-
-        jSeparator1 = new JSeparator();
-        jSeparator1.setPreferredSize(new java.awt.Dimension(200, 10));
+        writtenWorksLabel = new JLabel("Written Work (WW)");
+        writtenWorksLabel.setPreferredSize(new java.awt.Dimension(130, 16));
         
-        jLabel3 = new JLabel("Written Works (WW)");
-        jLabel3.setPreferredSize(new java.awt.Dimension(130, 16));
-        
-        jTextField1 = new JTextField();
-        jTextField1.setPreferredSize(new java.awt.Dimension(50, 22));
+        wwWeightField = new JTextField();
+        wwWeightField.setPreferredSize(new java.awt.Dimension(50, 22));
         
         jLabel19 = new JLabel("%");
 
-        jLabel12 = new JLabel("Performance Task (PT)");
-        jLabel12.setPreferredSize(new java.awt.Dimension(130, 16));
+        performanceTaskLabel = new JLabel("Performance Task (PT)");
+        performanceTaskLabel.setPreferredSize(new java.awt.Dimension(130, 16));
         
-        jTextField7 = new JTextField();
-        jTextField7.setPreferredSize(new java.awt.Dimension(50, 22));
+        ptWeightField = new JTextField();
+        ptWeightField.setPreferredSize(new java.awt.Dimension(50, 22));
         
         jLabel17 = new JLabel("%");
 
-        jLabel13 = new JLabel("Quarterly Assessment (QA)");
-        jLabel13.setPreferredSize(new java.awt.Dimension(130, 16));
+        quarterlyAssessmentLabel = new JLabel("Quarterly Assessment (QA)");
+        quarterlyAssessmentLabel.setPreferredSize(new java.awt.Dimension(130, 16));
         
-        jTextField8 = new JTextField();
-        jTextField8.setPreferredSize(new java.awt.Dimension(50, 22));
+        qaWeightField = new JTextField();
+        qaWeightField.setPreferredSize(new java.awt.Dimension(50, 22));
         
         jLabel16 = new JLabel("%");
         
         jSeparator3 = new JSeparator();
         jSeparator3.setPreferredSize(new java.awt.Dimension(200, 10));
         
-        jButton1 = new JButton("OK");
+        confirmButton = new JButton("Confirm");
         
-        add(jLabel1);
-        add(jSeparator1);
-        add(jLabel3);
-        add(jTextField1);
+        add(writtenWorksLabel);
+        add(wwWeightField);
         add(jLabel19);
-        add(jLabel12);
-        add(jTextField7);
+        add(performanceTaskLabel);
+        add(ptWeightField);
         add(jLabel17);
-        add(jLabel13);
-        add(jTextField8);
+        add(quarterlyAssessmentLabel);
+        add(qaWeightField);
         add(jLabel16);
         add(jSeparator3);
-        add(jButton1);
+        add(confirmButton);
         
+        setTitle("Set grade weights for this class record:");
         setSize(240, 200);
         setLocationRelativeTo(null);
     }
     
+    public String getWwWeightField() {
+        return wwWeightField.getText();
+    }
+
+    // Getter for ptWeightField
+    public String getPtWeightField() {
+        return ptWeightField.getText();
+    }
+
+    // Getter for qaWeightField
+    public String getQaWeightField() {
+        return qaWeightField.getText();
+    }
+
     public JButton getButton() {
-        return jButton1;
+        return confirmButton;
     }
 }
